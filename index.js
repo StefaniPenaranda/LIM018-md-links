821ecbf98d15bbae56cd81ae414f9beb4af803aa
@StefaniPenaranda <pstefanipm@gmail.com>
2022-09-14 09:21:14 -0500

    funciones readPath y test

--- index.js
+++ index.js
@@ -1,23 +1,65 @@
 const path = require('node:path');
 const fs = require("fs");
 const route = './README.md';
-
+const mdPruebaAbsoluta = 'C:/Users/STEFANI/desktop/md-links/LIM018-md-links/prueba_1/primero.md';
+const mdPruebaRelative = 'LIM018-md-links/prueba_1/primero.md';
 // console.log(process.argv[2])
 // console.log(__dirname)
 // console.log(path);
 
-// Validando que la ruta EXISTA------------------------------------------
-const routeValidator = (router) => {
- if (fs.existsSync(router)===true){
-  console.log('The path exists.')
-} else {
-    console.log ('The path no exists')
-  // return  fs.existsSync(router)
+// 1.-  Validando que la ruta EXISTA------------------------------------------
+// const pathExits = (router) => fs.existsSync(router);
+const pathExits = (router) =>{ // retorn TRUE or FALSE
+  if(typeof router !== 'string'){
+    console.log("la ruta esta vacia")
+    return false
+  }
+  return fs.existsSync(router)
+  };
+
+pathExits(route);
+
+// 2.- Validando si es absoluta o realtivThe path no existsa-----------------------
+// const aBsolute = path.isAbsolute(route); devuelve un booleano
+const convertToAbsolut = (router)=>{
+  if(path.isAbsolute(router)){
+    return router;
+    // return console.log(router);
+  }
+  else {
+    return path.resolve(router);
+    // return console.log(path.resolve(router));
+  }
+};
+convertToAbsolut(mdPruebaRelative);
+// Verifico si es un directorio o no
+//------------ se hace despues-------------;
+
+//3.-  Me indica el extencion que tiene la ruta --------------------------------
+const readMd = (router) =>{
+  return path.extname(router);
+  // console.log(path.extname(router));
+};
+readMd(route);
+
+//4.- lee el archivo
+// Con esta fubciob leo el archivo
+const readPath = (archivo) => {
+  console.log(fs.readFileSync(archivo,'utf-8'));
+}
+readPath(mdPruebaAbsoluta)
+
+//5.- 
+
+module.exports = {
+  pathExits,
+  convertToAbsolut,
+  readMd,
+  readPath,
 }
 
-}
-routeValidator(route);
 
+//--------------------------------------------------
 // Validando que la ruta existe sin SYNC
 // const routerValidatory = ( path) => {
 //   fs.exists(path,(e) =>{
@@ -26,35 +68,10 @@ routeValidator(route);
 // }
 // routerValidatory(route, console.log)
 
-// Validando si es absoluta o realtivThe path no existsa------------------------------------------------
-// const aBsolute = path.isAbsolute(route); devuelve un booleano
-const convertToAbsolut = (router)=>{
-  if(path.isAbsolute(router)){
-    // return router;
-    return console.log(router);
-  }
-  else {
-    // return path.resolve(router);
-    return console.log(path.resolve(router));
-  }
-}
-convertToAbsolut(route);
-
-// Me indica el extencion que tiene la ruta
-const prueba = (router) =>{
-  console.log(path.extname(router));
-};
-prueba(route);
-
-// const typeExtencionMd = (router)=>{
-
-// }
-// //leer data sin el SYNC
+//leer data sin el SYNC
 // const reedData = (path, callBack) =>{
 //   fs.readFile(path,(err,data)=>{
 //     callBack(data.toString());
 //   })
 // }
 // reedData(route,console.log);
-
-//
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,37 @@
-const mdLinks = require('../');
+// const mdLinks = require('../index.js');
+const { pathExits,convertToAbsolut , readMd  } = require ('../index.js');
+const route = './README.md';
+const falseRoute = './REAE.md';
+const number = 12345;
+const mdPruebaAbsoluta = 'C:/Users/STEFANI/desktop/md-links/LIM018-md-links/prueba_1/primero.md';
+const mdPruebaRelative = 'LIM018-md-links/prueba_1/primero.md';
+// const md = '.md'
 
-
-describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
+describe ('pruebas de la función pathExits', () => {
+  it ('Si no es un string debe retornar false', () => {
+    expect(pathExits(number)).toBe(false);
   });
-
+  it('Debe retornar TRUE si la ruta existe', () => {
+    expect (pathExits(route)).toBe(true);
+  });
+  it( 'Debe retornar FALSE si la ruta no existe', () => {
+    expect (pathExits(falseRoute)).toBe(false);
+  });
 });
+
+describe('Con convertToAbsolut comprueba si la ruta es absoluta y si no lo es la convierte a absoluta', () => {
+  it ('Reconoce si la ruta es absoluta y si lo es devuelve la ruta', () => {
+    expect (convertToAbsolut(mdPruebaAbsoluta)).toBe(mdPruebaAbsoluta);
+  })
+  it ( 'si no es absoluta la convierte a absoluta', () => {
+    expect (convertToAbsolut(mdPruebaRelative)).toBe(mdPruebaAbsoluta);
+  })
+});
+
+describe('Con readMd me devuele el tipo de extensión del archivo', () => {
+  it ('me devuelve el tipo de extensión que tiene el archivo', () => [
+    expect ( readMd(mdPruebaRelative)).toBe('.md')
+  ])
+
+})
+
